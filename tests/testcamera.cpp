5dc0396b7ce1927fc20d7f7e1e9147560d3d72bd
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2025-07-21 09:52:48 -0300

    com imshow

--- tests/testcamera.cpp
+++ tests/testcamera.cpp
@@ -2,23 +2,30 @@
 #include <iostream>
 
 int main() {
-    cv::VideoCapture cap("/dev/video10"); // 0 == /dev/video0
+    cv::VideoCapture cap("/dev/video10"); // câmera virtual v4l2loopback
 
     if (!cap.isOpened()) {
-        std::cerr << "Não foi possível abrir a câmera!" << std::endl;
+        std::cerr << "Erro ao abrir a câmera (/dev/video10)!" << std::endl;
         return -1;
     }
 
-    cv::Mat frame;
-    std::cout << "Pressione ESC para sair." << std::endl;
+    std::cout << "Câmera aberta. Pressione ESC para sair.\n";
 
+    cv::Mat frame;
     while (true) {
         cap >> frame;
-        if (frame.empty()) break;
+        if (frame.empty()) {
+            std::cerr << "Frame vazio!\n";
+            continue;
+        }
 
-        cv::imshow("Câmera", frame);
-        if (cv::waitKey(30) == 27) break; // ESC
+        cv::imshow("Câmera Virtual", frame);
+
+        // Espera 1ms por tecla, sai se pressionar ESC
+        if (cv::waitKey(1) == 27) break;
     }
 
+    cap.release();
+    cv::destroyAllWindows();
     return 0;
 }
