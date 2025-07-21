d900e30597ff0bab8e33c1a3e2f5ccd192864d8b
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2025-07-21 09:40:20 -0300

    testnewup v4

--- tests/testcamera.cpp
+++ tests/testcamera.cpp
@@ -5,14 +5,20 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <linux/videodev2.h>
+#include <cstring>
 
 using namespace std;
 
+struct Buffer {
+    void* start;
+    size_t length;
+};
+
 int main() {
-    // Abrir dispositivo
-    int fd = open("/dev/video0", O_RDWR);
+    const char* device = "/dev/video0";
+    int fd = open(device, O_RDWR);
     if (fd < 0) {
-        cerr << "Erro ao abrir dispositivo" << endl;
+        cerr << "Erro ao abrir dispositivo: " << device << endl;
         return 1;
     }
 
@@ -42,15 +48,48 @@ int main() {
         return 1;
     }
 
-    // Capturar um frame
-    v4l2_buffer buffer{};
-    buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buffer.memory = V4L2_MEMORY_MMAP;
-    buffer.index = 0;
+    // Mapear os buffers
+    Buffer* buffers = new Buffer[req.count];
+    for (unsigned int i = 0; i < req.count; ++i) {
+        v4l2_buffer buffer{};
+        buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buffer.memory = V4L2_MEMORY_MMAP;
+        buffer.index = i;
 
-    if (ioctl(fd, VIDIOC_QBUF, &buffer) < 0) {
+        if (ioctl(fd, VIDIOC_QUERYBUF, &buffer) < 0) {
+            cerr << "Erro ao consultar buffer" << endl;
+            close(fd);
+            delete[] buffers;
+            return 1;
+        }
+
+        buffers[i].length = buffer.length;
+        buffers[i].start = mmap(nullptr, buffer.length, 
+                               PROT_READ | PROT_WRITE, 
+                               MAP_SHARED, 
+                               fd, buffer.m.offset);
+
+        if (buffers[i].start == MAP_FAILED) {
+            cerr << "Erro ao mapear buffer" << endl;
+            close(fd);
+            delete[] buffers;
+            return 1;
+        }
+    }
+
+    // Enfileirar um buffer
+    v4l2_buffer buf{};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+
+    if (ioctl(fd, VIDIOC_QBUF, &buf) < 0) {
         cerr << "Erro ao enfileirar buffer" << endl;
         close(fd);
+        for (unsigned int i = 0; i < req.count; ++i) {
+            munmap(buffers[i].start, buffers[i].length);
+        }
+        delete[] buffers;
         return 1;
     }
 
@@ -59,24 +98,55 @@ int main() {
     if (ioctl(fd, VIDIOC_STREAMON, &type) < 0) {
         cerr << "Erro ao iniciar captura" << endl;
         close(fd);
+        for (unsigned int i = 0; i < req.count; ++i) {
+            munmap(buffers[i].start, buffers[i].length);
+        }
+        delete[] buffers;
         return 1;
     }
 
     // Aguardar frame
-    if (ioctl(fd, VIDIOC_DQBUF, &buffer) < 0) {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    timeval tv{};
+    tv.tv_sec = 5; // Timeout de 5 segundos
+
+    int r = select(fd + 1, &fds, nullptr, nullptr, &tv);
+    if (r <= 0) {
+        cerr << "Timeout ou erro ao aguardar frame" << endl;
+        close(fd);
+        for (unsigned int i = 0; i < req.count; ++i) {
+            munmap(buffers[i].start, buffers[i].length);
+        }
+        delete[] buffers;
+        return 1;
+    }
+
+    // Capturar frame
+    if (ioctl(fd, VIDIOC_DQBUF, &buf) < 0) {
         cerr << "Erro ao capturar frame" << endl;
         close(fd);
+        for (unsigned int i = 0; i < req.count; ++i) {
+            munmap(buffers[i].start, buffers[i].length);
+        }
+        delete[] buffers;
         return 1;
     }
 
     // Salvar imagem
     ofstream out("captura.jpg", ios::binary);
-    out.write(static_cast<char*>(buffer.m.userptr), buffer.bytesused);
+    out.write(static_cast<char*>(buffers[buf.index].start), buf.bytesused);
     out.close();
 
-    cout << "Imagem salva como captura.jpg" << endl;
+    cout << "Imagem salva como captura.jpg (" << buf.bytesused << " bytes)" << endl;
 
     // Limpeza
     close(fd);
+    for (unsigned int i = 0; i < req.count; ++i) {
+        munmap(buffers[i].start, buffers[i].length);
+    }
+    delete[] buffers;
+    
     return 0;
 }
