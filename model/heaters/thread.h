8deb1faf914d1044cecff786c4c1729d86147208
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2025-09-24 11:26:02 -0300

    classe threads

---
+++ model/heaters/thread.h
@@ -0,0 +1,23 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <thread>
+#include <functional>
+#include <atomic>
+
+class Thread {
+private:
+    std::thread t;
+    std::function<void()> func;
+    std::atomic<bool> running;
+
+public:
+    Thread();
+    ~Thread();
+
+    void execute(std::function<void()> f);
+    void stop();
+    bool isRunning() const;
+};
+
+#endif // THREAD_H
