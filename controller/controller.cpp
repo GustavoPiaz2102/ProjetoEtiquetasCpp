8400ca71e7fc3692700bf78ad6c1f0eb2686530a
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2025-12-22 07:45:02 -0300

    Alteração de condicional de impressão

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -137,17 +137,19 @@ void Controller::run()
                 if (interface.config_impress(qnt_impress, &InstantImpress))
                 {
                     selected_option = -1;
+                    interface.setImprimindo(true);
+
                 }
                 imp.setQntImpressao(qnt_impress);
+
                 if (InstantImpress && qnt_impress > 0)
                 {
-                    interface.setImprimindo(true);
                     for (int i = 0; i < qnt_impress; i++)
                     {
                         imp.print();
                     }
+                    interface.setImprimindo(false);
                 }
-                interface.setImprimindo(false);
                 break;
             }
             //
