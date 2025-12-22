0cabc2577e5c63b888dadf3b2415b56f65be4a53
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2025-12-22 09:41:04 -0300

    Re-Return

--- controller/detector.cpp
+++ controller/detector.cpp
@@ -1,7 +1,7 @@
 #include "detector.h"
 
 Detector::Detector(Impress &imp, Interface &interface, Validator &validator)
-    : camera(0), ocr("eng"), sensor(19, 18,"gpiochip4"), imp(imp), interface(interface), validator(validator) {}
+    : camera(0), ocr("eng"), sensor(19, 18, "gpiochip4"), imp(imp), interface(interface), validator(validator) {}
 
 Detector::~Detector()
 {
@@ -90,19 +90,16 @@ void Detector::SensorCaptureImpressTHR()
                 }
                 else
                 {
-                    if (interface.PopUpError("Erro ao iniciar a impressão."))
+
+                    if (imp.getQntImpressao() <= 0)
+                    {
+                        interface.setImprimindo(false);
+                        running = false; // ✅ pode parar o loop
+                    }
+                    
+                    else
                     {
                         imp.setLastImpress(true);
-
-                        if (imp.getQntImpressao() <= 0)
-                        {
-                            interface.setImprimindo(false);
-                            running = false; // ✅ pode parar o loop
-                        }
-                        else
-                        {
-                            imp.setLastImpress(true);
-                        }
                     }
                 }
 
