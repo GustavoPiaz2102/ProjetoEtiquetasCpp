5590c92094df8ac2323c89909832a8057a185647
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-01-08 10:32:43 -0300

    File Way att

--- model/implement/capture.cpp
+++ model/implement/capture.cpp
@@ -48,7 +48,7 @@ cv::Mat Capture::captureImage() {
         return cv::Mat();
     }
     //salvar imagem para debug
-    std::string filename = "debug_capture(" + std::to_string(debug_counter) + ").jpg";
+    std::string filename = "../../DebugImages/debug_capture(" + std::to_string(debug_counter) + ").jpg";
     cv::imwrite(filename, frame);
     debug_counter++;
     //cv::flip(frame, frame, -1);
