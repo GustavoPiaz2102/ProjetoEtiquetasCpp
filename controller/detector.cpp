025bc04f1fba41a893f327b25567b28bf42bf60d
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-02-09 14:29:35 -0300

    realloc line

--- controller/detector.cpp
+++ controller/detector.cpp
@@ -130,9 +130,8 @@ void Detector::SensorCaptureImpressTHR()
                 // 3. Marca erro na impressão
                 imp.setLastImpress(true);
             }
-
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         else
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
