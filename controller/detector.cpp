17ef35361442bb6ea9f6491f74d2b331954a0b16
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-02-09 18:20:59 -0300

    gkjdahkgadflgjaçljl

--- controller/detector.cpp
+++ controller/detector.cpp
@@ -91,8 +91,9 @@ void Detector::SensorCaptureImpressTHR()
         if (sensor.ReadSensor() != 0)
         {
             sensor.SetStroboHigh();
-            std::this_thread::sleep_for(std::chrono::milliseconds(80));
+            std::this_thread::sleep_for(std::chrono::milliseconds(40));
             cv::Mat newFrame = camera.captureImage();
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
             sensor.SetStroboLow();
 
             {
