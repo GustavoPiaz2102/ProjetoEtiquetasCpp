9b49e8de55aa4c2fa4ca4c2269eed7796814ba53
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-02-13 15:40:18 -0300

    troca de debouce

--- model/heaters/GPIO.h
+++ model/heaters/GPIO.h
@@ -14,7 +14,7 @@ const std::string FILE_RAW   = DEVICE_DIR + "in_voltage0_raw";
 const std::string FILE_SCALE = DEVICE_DIR + "in_voltage0_scale"; 
 
 #define SENSOR_THRESHOLD 16200
-#define DebounceValue 5
+#define DebounceValue 10
 
 class GPIO{
 	private:
