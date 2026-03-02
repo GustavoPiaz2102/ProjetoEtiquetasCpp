06ebb20d94434d439ae363e77e5eda498461452d
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-03-02 14:38:32 -0300

    troca de valor de Hysteresis

--- model/heaters/GPIO.h
+++ model/heaters/GPIO.h
@@ -14,7 +14,7 @@ const std::string FILE_RAW   = DEVICE_DIR + "in_voltage0_raw";
 const std::string FILE_SCALE = DEVICE_DIR + "in_voltage0_scale"; 
 
 #define SENSOR_THRESHOLD 16200
-#define SENSOR_HYSTERESIS 200 //(16k desliga e 16400 liga)
+#define SENSOR_HYSTERESIS 100 //(16k desliga e 16400 liga)
 #define DEBOUNCE_MS 30 //periodo entre leituras
 #define FILTER_ALPHA 0.25
 // cada iteração contribui em 25% do valor final (aumentar se ficar muito suave)
