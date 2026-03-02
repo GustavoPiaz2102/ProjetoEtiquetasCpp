286daa33a431eec3ef5b79018001cdc51822b897
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-03-02 17:41:45 -0300

    remove coment

--- model/implement/OCR.cpp
+++ model/implement/OCR.cpp
@@ -21,29 +21,15 @@ std::string OCR::extractText(const cv::Mat& inputImage){
 		return "";
 	}
 
-	// Garantir que a imagem seja contínua
 	cv::Mat img = inputImage.isContinuous() ? inputImage : inputImage.clone();
 
-	// Converter para grayscale se tiver 3 canais
 	cv::Mat gray;
 	if (img.channels() == 3) cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 	else gray = img;
 
-	// Ajuste mínimo de contraste
-	//cv::Mat contrast;
-	//cv::normalize(gray, contrast, 0, 255, cv::NORM_MINMAX);
-
-	// Leve nitidez
-	//cv::Mat kernel = (cv::Mat_<float>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-	//cv::filter2D(contrast, contrast, contrast.depth(), kernel);
-
-	// Passar para Tesseract
-	//tess->SetImage(contrast.data, contrast.cols, contrast.rows, 1, contrast.step);
 	tess->SetImage(gray.data, gray.cols, gray.rows, 1, gray.step);
-	// PSM adequado para múltiplas linhas de um bloco
 	tess->SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
 
-	// Reconhece a imagem
 	tess->Recognize(0);
 
 	std::string finalText;
