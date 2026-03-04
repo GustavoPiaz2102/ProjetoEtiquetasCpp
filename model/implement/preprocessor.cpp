da7fe28a65bb32a5c720afa49cd201e585c9135a
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-03-04 15:17:00 -0300

    troca de normalização

--- model/implement/preprocessor.cpp
+++ model/implement/preprocessor.cpp
@@ -36,8 +36,19 @@ cv::Mat Preprocessor::prepareForRec(const cv::Mat& inputImage) {
 	else
 		cv::cvtColor(inputImage, rgb, cv::COLOR_BGR2RGB);
 
+	cv::Mat floatImg;
+	rgb.convertTo(floatImg, CV_32F, 1.0 / 255.0);
+
+	// Normalização ImageNet: mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]
+	std::vector<cv::Mat> channels(3);
+	cv::split(floatImg, channels);
+
+	channels[0] = (channels[0] - 0.485f) / 0.229f;
+	channels[1] = (channels[1] - 0.456f) / 0.224f;
+	channels[2] = (channels[2] - 0.406f) / 0.225f;
+
 	cv::Mat result;
-	rgb.convertTo(result, CV_32F, 1.0 / 127.5, -1.0);
+	cv::merge(channels, result);
 
 	return result;
 }
