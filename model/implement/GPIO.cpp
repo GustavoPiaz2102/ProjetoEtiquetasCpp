e545a4830dafa32fc94fc21a269af1598860c7cd
GustavoPiaz2102 <gustavopiazdasilva2102@gmail.com>
2026-07-23 14:00:49 -0300

    aumento do tempo do strobo

--- model/implement/GPIO.cpp
+++ model/implement/GPIO.cpp
@@ -86,7 +86,7 @@ int GPIO::ReadRaw() {
 
 bool GPIO::ReadSensor() {
 	int rawValue = ReadRaw();
-	std::cout << "valor lido:" << rawValue;
+	std::cout << "valor lido:" << rawValue << std::endl;
 	if (rawValue < 0)
 		return false;
 
